a60bc17c69f8d7a35f9a3fa248a2490c83caf03f
gabuilders <gabriel.arthur@platformbuilders.io>
2025-09-30 03:15:52 -0300

    Mudando o ip

--- src/services/todosService.ts
+++ src/services/todosService.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { User, UserCreateData } from '../types';
 
 export const apiClient = axios.create({
-    baseURL: 'http://18.220.97.151:8080',
+    baseURL: 'http://3.14.86.74:8080',
     headers: {
         'Content-Type': 'application/json',
     },
